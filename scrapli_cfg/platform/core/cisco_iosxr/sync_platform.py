d8d6f1fb44e5fa80e831bfae0f5701f08903cd9a
Martin Mitchell <mcmitch405@hotmail.com>
2024-09-26 12:27:56 +0100

    Add support for commit confirm on XR Sync

--- scrapli_cfg/platform/core/cisco_iosxr/sync_platform.py
+++ scrapli_cfg/platform/core/cisco_iosxr/sync_platform.py
@@ -139,14 +139,18 @@ class ScrapliCfgIOSXR(ScrapliCfgPlatform, ScrapliCfgIOSXRBase):
         )
 
         if self._replace is True:
-            commit_events = [("commit replace", "proceed?"), ("yes", "")]
+            commit_events = [("commit confirm replace", "proceed?"), ("yes", "")]
             commit_result = self.conn.send_interactive(
                 interact_events=commit_events, privilege_level=self._config_privilege_level
             )
         else:
-            commit_result = self.conn.send_config(config="commit")
+            commit_result = self.conn.send_config(config="commit confirm")
 
         scrapli_responses.append(commit_result)
+
+        confirm_result = self.conn.send_config(config="commit")
+        scrapli_responses.append(commit_result)
+        
         self._reset_config_session()
 
         return self._post_commit_config(response=response, scrapli_responses=scrapli_responses)
